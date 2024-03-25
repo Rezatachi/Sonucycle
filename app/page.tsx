25e63b602b354d09232fab24f33d0a15f5a6e1c4
Abraham Belayneh <abraham@caltech.edu>
2024-03-25 14:59:26 -0400

    Setup Next.js Project w/ Supabase

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import DeployButton from "../components/DeployButton";
+import AuthButton from "../components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
+import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
+import Header from "@/components/Header";
+
+export default async function Index() {
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <h1>hi</h1>
+    </div>
+  );
+}
